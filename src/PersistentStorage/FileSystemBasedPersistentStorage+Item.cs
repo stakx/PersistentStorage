21b11626caf8495dedd51f99c03754fcdd9f6ecc
stakx <stakx@eml.cc>
2018-04-19 21:42:35 +0200

    Remove superfluous `using` directives

--- src/PersistentStorage/FileSystemBasedPersistentStorage+Item.cs
+++ src/PersistentStorage/FileSystemBasedPersistentStorage+Item.cs
@@ -3,8 +3,6 @@
 // License available at https://github.com/stakx/PersistentStorage/blob/develop/LICENSE.md.
 #endregion
 
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
--- src/PersistentStorage/IPersistentStorageItem.cs
+++ src/PersistentStorage/IPersistentStorageItem.cs
@@ -3,7 +3,6 @@
 // License available at https://github.com/stakx/PersistentStorage/blob/develop/LICENSE.md.
 #endregion
 
-using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
